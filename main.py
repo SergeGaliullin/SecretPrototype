6ff38f06ecf38b64de63a763ddbe774b7ec10b6a
SergeGaliullin <sergei.galiullin@gmail.com>
2016-09-14 14:09:17 -0300

    Connected python and kivy files

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Label
 
 Builder.load_file('design.kv')
 
+class RootWidget(BoxLayout):
+    pass
+
 
 class TutorialApp(App):
     def build(self):
-        return Label(text='H')
+        return RootWidget()
 
 
 if __name__ == "__main__":
